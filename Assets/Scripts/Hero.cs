54169ae849170830be0271469787232c031a448f
Catomrad <91577742+Catomrad@users.noreply.github.com>
2024-04-22 19:51:58 +0700

    jump added

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -6,7 +6,7 @@ public class Hero : MonoBehaviour
 {
     [SerializeField] private float speed = 1.0f;
     [SerializeField] private int lives = 3;
-    [SerializeField] private float jumpForse = 15f;
+    [SerializeField] private float jumpForse = 5.0f;
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
@@ -20,11 +20,18 @@ public class Hero : MonoBehaviour
     {
         if (Input.GetButton("Horizontal"))
             Run();
+        if (Input.GetButton("Jump"))
+            Jump();
     }
     private void Run()
     {
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
         transform.position = Vector3.MoveTowards(transform.position,transform.position + dir, speed * Time.deltaTime);
-        sprite.sprite = "hero-frame-2";
+        sprite.flipX = !(dir.x > 0.0f);
+        //sprite.sprite = "hero-frame-2";
+    }
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForse, ForceMode2D.Impulse);
     }
 }
