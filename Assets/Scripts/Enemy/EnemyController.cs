60269e975aa6dc154f6dde3dbdef4111fcc2666a
artiartem <a_shitikov8@mail.ru>
2024-04-27 00:30:44 +0700

    State

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -3,13 +3,21 @@ using UnityEngine.Serialization;
 
 namespace Enemy
 {
+    public enum AIState
+    {
+        Idle,
+        Patrol,
+        Chasing
+    }
     public class EnemyController : MonoBehaviour
     {
         [Range(0f, 5f)] public float moveSpeed = 5.0f;
 
+        public AIState aiState = AIState.Patrol;
+        
         private Vector2 _moveDirection = Vector2.zero;
         private Rigidbody2D _rb;
-
+        
         // Start is called before the first frame update
         private void Start()
         {
@@ -33,6 +41,7 @@ namespace Enemy
             // Если вектор приблизительно вверх то прыжок
             if (Vector2.Dot(_moveDirection, Vector2.up) > 0.75)
             {
+                Debug.Log($"Jump {Vector2.Dot(_moveDirection, Vector2.up)}>0.75 ({_moveDirection})");
                 // TODO: Прыжок
             }
 
