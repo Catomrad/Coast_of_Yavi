3020fd55170a42d9fdcf2b60277bcca32898dbb4
Catomrad <91577742+Catomrad@users.noreply.github.com>
2024-04-28 20:03:19 +0700

    added CameraContolled and GetDamage fuction
    Добавил слежение камеры за игроком. Реализовал получение урона игроком

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,28 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Hero : MonoBehaviour
+public class Hero : Entity
 {
-    [SerializeField] private float speed = 1.0f;
-    [SerializeField] private int lives = 3;
-    [SerializeField] private float jumpForse = 5.0f;
+    [SerializeField] private float speed = 3.0f;
+    [SerializeField] private int health;
+    [SerializeField] private float jumpForce = 10f;
+    private bool isGrounded = false;
+
+    [SerializeField] private Image[] hearts;
+    [SerializeField] private Sprite aliveHeart;
+    [SerializeField] private Sprite deadHeart;
+
+    public bool isAttacking = false;
+    public bool isRecharged = true;
+
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask enemy;
+
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
 
+    public static Hero Instance { get; set; }
+
     private void Awake()
     {
+        lives = 3;
+        health = lives;
+        Instance = this;
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        isRecharged = true;
     }
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+    public override void GetDamage()
+    {
+        lives -= 1;
+        Debug.Log(lives);
+        if (lives < 1)
+        {
+            Die();
+        }
+    }
+
+    private void Attack()
+    {
+       
+    }
+
     private void Update()
     {
         if (Input.GetButton("Horizontal"))
             Run();
-        if (Input.GetButton("Jump"))
+        if (isGrounded && Input.GetButtonDown("Jump"))
             Jump();
+        
+        if(health > lives)
+        {
+            health = lives;
+        } 
+        for (int i = 0 ;i < hearts.Length ; i++) 
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = aliveHeart;
+            }
+            else
+                hearts[i].sprite = deadHeart;
+
+            if (i < lives)
+                hearts[i].enabled = true;
+            else 
+                hearts[i].enabled = false;
+        }
     }
+
     private void Run()
     {
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
@@ -32,6 +92,11 @@ public class Hero : MonoBehaviour
     }
     private void Jump()
     {
-        rb.AddForce(transform.up * jumpForse, ForceMode2D.Impulse);
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        isGrounded = collider.Length > 1;
     }
 }
