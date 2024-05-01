84eff2300df8a85b1116cb187ebce794a6af4077
artiartem <a_shitikov8@mail.ru>
2024-05-01 22:38:34 +0700

    Some refactoring
    немного переименовал переменные
    подправил функции

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -23,8 +23,8 @@ public class Hero : Entity
     public LayerMask enemy;
 
 
-    private Rigidbody2D rb;
-    private SpriteRenderer sprite;
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sprite;
 
     public static Hero Instance { get; set; }
 
@@ -33,14 +33,16 @@ public class Hero : Entity
         lives = 3;
         health = lives;
         Instance = this;
-        rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponentInChildren<SpriteRenderer>();
+        _rb = GetComponent<Rigidbody2D>();
+        _sprite = GetComponentInChildren<SpriteRenderer>();
         isRecharged = true;
     }
+
     private void FixedUpdate()
     {
         CheckGround();
     }
+
     public override void GetDamage()
     {
         lives -= 1;
@@ -53,7 +55,6 @@ public class Hero : Entity
 
     private void Attack()
     {
-       
     }
 
     private void Update()
@@ -62,41 +63,56 @@ public class Hero : Entity
             Run();
         if (isGrounded && Input.GetButtonDown("Jump"))
             Jump();
-        
-        if(health > lives)
-        {
-            health = lives;
-        } 
-        for (int i = 0 ;i < hearts.Length ; i++) 
+
+        if (health > lives) health = lives;
+
+        for (var i = 0; i < hearts.Length; i++)
         {
             if (i < health)
-            {
                 hearts[i].sprite = aliveHeart;
-            }
             else
                 hearts[i].sprite = deadHeart;
 
             if (i < lives)
                 hearts[i].enabled = true;
-            else 
+            else
                 hearts[i].enabled = true;
         }
     }
 
+
+    public override void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+    // TODO: Попробовать использовать (_rb) RigidBody2D.velocity для передвижения
+    // https://docs.unity3d.com/ScriptReference/Rigidbody2D-velocity.html
+
     private void Run()
     {
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
-        transform.position = Vector3.MoveTowards(transform.position,transform.position + dir, speed * Time.deltaTime);
-        sprite.flipX = !(dir.x > 0.0f);
+        var position = transform.position;
+        position = Vector3.MoveTowards(position, position + dir, speed * Time.deltaTime);
+        transform.position = position;
+        _sprite.flipX = !(dir.x > 0.0f);
         //sprite.sprite = "hero-frame-2";
     }
+
     private void Jump()
     {
-        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        _rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
+    
+    // немного переименовал переменные
+    //     подправил функции
+
     private void CheckGround()
     {
-        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
-        isGrounded = collider.Length > 1;
+        var pos = transform.position;
+        var results = new Collider2D[2];
+        var size = Physics2D.OverlapCircleNonAlloc(pos, 0.3f, results);
+        isGrounded = size > 1;
     }
 }
