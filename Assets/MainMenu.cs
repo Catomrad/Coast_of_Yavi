269d25afeb3a5c894af41a407ac0bf074df97148
artiartem <a_shitikov8@mail.ru>
2024-06-03 22:54:31 +0700

    Basic controls

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,21 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    private List<string> _levels = new() {"SampleScene", "Level2", "Level3", "Level4", "Level5"};
+
+    private void Start()
+    {
+        _levels = LoadLevels();
+    }
+
+    private static List<string> LoadLevels()
+    {
+        // not implemented
+        return new List<string> { "SampleScene" };
+    }
 
     // Начать игру
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(1); // File -> Build settings -> Scenes in Build
     }
     
 
     // Продолжить игру
-    public void Continue() { }
-    
-    // Бестиарий
-    public void Bestiary() { }
-    
-    // Настройки
-    public void Settings() { }
+    public void Continue()
+    {
+        SceneManager.LoadScene(_levels[~1]);
+    }
 }
